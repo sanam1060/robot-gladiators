b1d8d2df2df702d2b593721d2217e2f72df64089
sanam1060 <sanam_g1990@yahoo.com>
2022-07-21 12:07:03 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -35,14 +35,18 @@ var fight = function (enemyNames) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         
         //Subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
     
     //Remove enemy's health bt subtracting the amout set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    //Generate random damage value based on player's attack power
+    
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     
     console.log(playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining.");
 
@@ -61,7 +65,9 @@ var fight = function (enemyNames) {
     }
     
     //Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
   
     //Log a resulting message to the console so we know that it worked
     console.log(enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
@@ -102,7 +108,7 @@ var startGame = function (){
     var pickedEnemyName = enemyNames[i];
   
     //Reset enemyHealth before starting new fight
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
     
     //Use debugger to pause script from running and check what's going on at that moment in the code
   
@@ -200,6 +206,13 @@ var shop = function(){
       break;
   }
 
+};
+
+//Function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
 };
 
 //Start the game when the page loads
