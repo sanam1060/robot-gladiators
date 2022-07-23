ce053f848ca97f5291ed4d254f3ed36fa16b4fc8
sanam1060 <sanam_g1990@yahoo.com>
2022-07-22 22:01:15 -0400

    Save and Load the High Score

--- assets/js/game.js
+++ assets/js/game.js
@@ -51,13 +51,13 @@ var fight = function (enemy) {
   //Keep track of who goes first
   var isPlayerTurn = true;
 
-  if (Math.random > 0.5) {
-    isPlayerTurn = false
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
   }
 
   //Repeat and execute as long as the enemy-robot is alive
   while (playerInfo.health > 0 && enemy.health > 0) { 
-    if (isPlayerTurn()){
+    if (isPlayerTurn){
       
       //Ask player if they'd like to fight or skip using fightOrSkip function
       if (fightOrSkip()) {
@@ -171,11 +171,23 @@ var startGame = function (){
 //Function to end the entire game
 var endGame = function (){
 
-  //If player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survided the game! You now have a score of " + playerInfo.money + ".");
+  window.alert("The game has now ended. Let's see how you did!");
+
+  //Check local storage for high score, if it's not there, use 0
+
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+
+  //If player has more money than the high score, plater has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
   } else {
-    window.alert("You've lost your robot in battle.");
+    alert(playerInfo.name + "did not beat the high score of " + highScore + ". Maybe next time!");
   }
 
   //Ask player if they'd like to play again
